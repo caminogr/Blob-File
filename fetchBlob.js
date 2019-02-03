f18f93591bb017ec1197b001c29ef7145d1ff846
kaminora <kaminoura9207@gmail.com>
2019-02-04 00:03:51 +0900

    Add fetch function

--- fetchBlob.js
+++ fetchBlob.js
@@ -1,7 +1,8 @@
 document.getElementById("file").addEventListener("change", function() {
   // console.log("this", this.files[0]);
   // const file = this.files[0];
-  callXhr()
+  // callXhr()
+  callFetch();
 });
 
 const callXhr = function() {
@@ -18,4 +19,19 @@ const callXhr = function() {
   xhr.open('GET', '/');
   xhr.send(null);
   console.log('finish!!!')
+}
+
+const callFetch = function() {
+  fetch("/", {
+    method: "GET",
+  }).then(response => {
+    console.log('response1', response);
+    const blob = response.blob();
+    console.log(response.status); // status
+    console.log('blob', blob)
+    return blob;
+  })
+  .then(blob => {
+    console.log('blob', blob);
+  });
 }
